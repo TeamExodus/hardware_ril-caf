CyanogenMod/android_hardware_ril

RF cutback feature for SAR
Change-Id: I1d79d00a3ac177633253108f801967f1042d76c5
Issue-id:FEIJ-350

libril/ril.cpp
@@ -272,6 +272,7 @@ static void dispatchSimAuthentication(Parcel &p, RequestInfo *pRI);
 static void dispatchDataProfile(Parcel &p, RequestInfo *pRI);
 static void dispatchRadioCapability(Parcel &p, RequestInfo *pRI);
 static void dispatchOpenChannelWithP2(Parcel &p, RequestInfo *pRI);
+static void dispatchsetMaxTransmitPower(Parcel &p, RequestInfo *pRI);
 static int responseInts(Parcel &p, void *response, size_t responselen);
 static int responseFailCause(Parcel &p, void *response, size_t responselen);
 static int responseStrings(Parcel &p, void *response, size_t responselen);
@@ -516,7 +517,7 @@ processCommandBuffer(void *buffer, size_t buflen, RIL_SOCKET_ID socket_id) {
 
     if (request < 1 || request >= (int32_t)NUM_ELEMS(s_commands)) {
         Parcel pErr;
-        RLOGE("unsupported request code %d token %d", request, token);
+        RLOGE("unsupported request code %d token %d num_elem %d", request, token, (int32_t)NUM_ELEMS(s_commands));
         // FIXME this should perhaps return a response
         pErr.writeInt32 (RESPONSE_SOLICITED);
         pErr.writeInt32 (token);
@@ -2098,6 +2099,38 @@ static void dispatchOpenChannelWithP2 (Parcel &p, RequestInfo *pRI) {
 
     return;
 invalid:
+    invalidCommandBlock(pRI);
+    return;
+}
+
+
+static void dispatchsetMaxTransmitPower(Parcel &p, RequestInfo *pRI) {
+    rf_control_state state;
+    int32_t t;
+    status_t status;
+
+    memset(&state, 0, sizeof(state));
+    status = p.readInt32(&t);
+    if (status != NO_ERROR) {
+        RLOGE("dispatchsetMaxTransmitPower ERROR");
+        goto invalid;
+    }
+    state.state = (int)t;
+    RLOGI("dispatchsetMaxTransmitPower: %d\n", state.state);
+
+    startRequest;
+    appendPrintBuf("%sstate.state=%d", printBuf, state.state);
+    closeRequest;
+    printRequest(pRI->token, pRI->pCI->requestNumber);
+
+    s_callbacks.onRequest(pRI->pCI->requestNumber, &state, sizeof(state), pRI);
+
+    #ifdef MEMSET_FREED
+    memset(&state, 0, sizeof(state));
+    #endif
+
+    return;
+    invalid:
     invalidCommandBlock(pRI);
     return;
 }
@@ -5228,6 +5261,7 @@ requestToString(int request) {
         case RIL_REQUEST_GET_DC_RT_INFO: return "GET_DC_RT_INFO";
         case RIL_REQUEST_SET_DC_RT_INFO_RATE: return "SET_DC_RT_INFO_RATE";
         case RIL_REQUEST_SET_DATA_PROFILE: return "SET_DATA_PROFILE";
+        case RIL_REQUEST_SET_MAX_TRANSMIT_POWER: return "RIL_REQUEST_SET_MAX_TRANSMIT_POWER";
         case RIL_UNSOL_RESPONSE_RADIO_STATE_CHANGED: return "UNSOL_RESPONSE_RADIO_STATE_CHANGED";
         case RIL_UNSOL_RESPONSE_CALL_STATE_CHANGED: return "UNSOL_RESPONSE_CALL_STATE_CHANGED";
         case RIL_UNSOL_RESPONSE_VOICE_NETWORK_STATE_CHANGED: return "UNSOL_RESPONSE_VOICE_NETWORK_STATE_CHANGED";

TeamExodus/hardware_ril-caf

Change-Id: I0cda490538cdf14ebf5248c5c487677e71a6ac07

libril/ril.cpp
@@ -272,6 +272,7 @@ static void dispatchSimAuthentication(Parcel &p, RequestInfo *pRI);
 static void dispatchDataProfile(Parcel &p, RequestInfo *pRI);
 static void dispatchRadioCapability(Parcel &p, RequestInfo *pRI);
 static void dispatchOpenChannelWithP2(Parcel &p, RequestInfo *pRI);
+static void dispatchSetMaxTransmitPower(Parcel &p, RequestInfo *pRI);
 static int responseInts(Parcel &p, void *response, size_t responselen);
 static int responseFailCause(Parcel &p, void *response, size_t responselen);
 static int responseStrings(Parcel &p, void *response, size_t responselen);
@@ -2094,6 +2095,38 @@ static void dispatchOpenChannelWithP2 (Parcel &p, RequestInfo *pRI) {
 
 #ifdef MEMSET_FREED
     memset(&openChannel, 0, sizeof(openChannel));
+#endif
+
+    return;
+invalid:
+    invalidCommandBlock(pRI);
+    return;
+}
+
+
+static void dispatchSetMaxTransmitPower(Parcel &p, RequestInfo *pRI) {
+    RIL_RfControlState state;
+    int32_t t;
+    status_t status;
+
+    memset(&state, 0, sizeof(state));
+    status = p.readInt32(&t);
+    if (status != NO_ERROR) {
+        RLOGE("__func__ ERROR");
+        goto invalid;
+    }
+    state.state = (int)t;
+    RLOGI("__func__ : %d\n", state.state);
+
+    startRequest;
+    appendPrintBuf("%sstate.state=%d", printBuf, state.state);
+    closeRequest;
+    printRequest(pRI->token, pRI->pCI->requestNumber);
+
+    CALL_ONREQUEST(pRI->pCI->requestNumber, &state, sizeof(state), pRI, pRI->socket_id);
+
+#ifdef MEMSET_FREED
+    memset(&state, 0, sizeof(state));
 #endif
 
     return;
@@ -5228,6 +5261,7 @@ requestToString(int request) {
         case RIL_REQUEST_GET_DC_RT_INFO: return "GET_DC_RT_INFO";
         case RIL_REQUEST_SET_DC_RT_INFO_RATE: return "SET_DC_RT_INFO_RATE";
         case RIL_REQUEST_SET_DATA_PROFILE: return "SET_DATA_PROFILE";
+        case RIL_REQUEST_SET_MAX_TRANSMIT_POWER: return "RIL_REQUEST_SET_MAX_TRANSMIT_POWER";
         case RIL_UNSOL_RESPONSE_RADIO_STATE_CHANGED: return "UNSOL_RESPONSE_RADIO_STATE_CHANGED";
         case RIL_UNSOL_RESPONSE_CALL_STATE_CHANGED: return "UNSOL_RESPONSE_CALL_STATE_CHANGED";
         case RIL_UNSOL_RESPONSE_VOICE_NETWORK_STATE_CHANGED: return "UNSOL_RESPONSE_VOICE_NETWORK_STATE_CHANGED";